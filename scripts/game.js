acc710e2d6149191278d2f3d2fbe511cc9b9ab0e
AnnaDobrucki <anna.dobrucki@hotmail.co.uk>
2023-07-25 08:52:35 +0000

    changes and additions

--- scripts/game.js
+++ scripts/game.js
@@ -5,6 +5,7 @@ let game = {
     currentGame: [],
     playerMoves: [],
     score: 0,
+    turnNumber: 0,
     choices: ["button1", "button2", "button3", "button4"]
 };
 
@@ -12,17 +13,42 @@ function newGame() {
     game.currentGame = [];
     game.playerMoves = [];
     game.score = 0;
+
+    for (let circle of document.getElementsByClassName("circle")) {
+        if (circle.getAttribute("data-listener") !== "true") {
+            circle.addEventListener("click", (e) => {
+                if (game.currentGame.length > 0 && !game.turnInProgress){}
+                if (game.currentGame.length > 0){
+                let move = e.target.getAttribute("id");
+                game.lastbutton = move;
+                game.playerMoves.push(move);
+                lightsOn(move);
+                playerTurn();}}
+            });
+            circle.setAttribute("data-listener", "true");
+        }
+    }
     showScore();
     addTurn();
 }
-function addTurn(){
+
+function addTurn() {
     game.playerMoves = [];
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
-    // showTurns();
+    showTurns();
 }
 
-function showScore() {
-    document.getElementById("score").innerText = game.score;
+function showTurns() {
+    game.turnInProgress = true
+    game.turnNumber = 0;
+    let turns = setInterval(function () {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+            game.turnInProgress = false;
+        }
+    }, 800);
 }
 
 function lightsOn(circ) {
@@ -32,4 +58,23 @@ function lightsOn(circ) {
     }, 400);
 }
 
-module.exports = { game, newGame, showScore, addTurn,lightsOn };
+function playerTurn() {
+    let i = game.playerMoves.length - 1;
+    if (game.currentGame[i] === game.playerMoves[i]) {
+        if (game.currentGame.length === game.playerMoves.length) {
+            game.score++;
+            showScore();
+            addTurn();
+        }
+        else {
+            alert("Wrong Move!");
+            newGame();
+        }
+    } 
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn };
